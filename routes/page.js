bc750f5cdc41820872aba979561c76913ee77363
geonryu <60060831+geonryu@users.noreply.github.com>
2023-09-30 22:06:14 +0900

    org add

--- routes/page.js
+++ routes/page.js
@@ -3,9 +3,20 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.render("index", {
-    title: "매직넘버",
-  });
+    res.render("index", {
+        title: "한국접근성평가연구원",
+    });
+}); // 랜딩 페이지
+
+router.get("/intro", async (req, res, next) => {
+    res.render("intro", {
+        title: "한국접근성평가연구원",
+    });
+}); // 랜딩 페이지
+router.get("/org", async (req, res, next) => {
+    res.render("org", {
+        title: "한국접근성평가연구원",
+    });
 }); // 랜딩 페이지
 
 module.exports = router;
