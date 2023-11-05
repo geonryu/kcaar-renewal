19604c271479172c2b167c9fa7e531a41ef68d0c
geonryu <60060831+geonryu@users.noreply.github.com>
2023-11-05 16:47:52 +0900

    페이지 추가

--- routes/page.js
+++ routes/page.js
@@ -12,11 +12,49 @@ router.get("/intro", async (req, res, next) => {
     res.render("intro", {
         title: "한국접근성평가연구원",
     });
-}); // 랜딩 페이지
+}); // 연구원소개
 router.get("/org", async (req, res, next) => {
     res.render("org", {
         title: "한국접근성평가연구원",
     });
-}); // 랜딩 페이지
+}); // 조직도 안내
+router.get("/center", async (req, res, next) => {
+    res.render("center", {
+        title: "한국접근성평가연구원",
+    });
+}); // 조직도 안내
+router.get("/appliances", async (req, res, next) => {
+    res.render("appliances", {
+        title: "한국접근성평가연구원",
+    });
+}); // 접근성시험평가 안내
+router.get("/kiosk", async (req, res, next) => {
+    res.render("kiosk", {
+        title: "한국접근성평가연구원",
+    });
+}); // 접근성시험평가 안내
+router.get("/login", async (req, res, next) => {
+    res.render("login", {
+        title: "한국접근성평가연구원",
+    });
+}); // 로그인
+router.get("/join", async (req, res, next) => {
+    res.render("join", {
+        title: "한국접근성평가연구원",
+    });
+}); // sign up
+router.get("/notice", async (req, res, next) => {
+    res.render("notice", {
+        title: "한국접근성평가연구원",
+    });
+}); // sign up
+router.get("/contact", async (req, res, next) => {
+    res.render("contact", {
+        title: "한국접근성평가연구원",
+    });
+}); // sign up
+
+
+
 
 module.exports = router;
